21e81d25c9596fbbe0e5cf904aa188a2ef44c24c
yaaroub <yarobdaoud1997@gmail.com>
2022-06-30 14:55:04 +0200

    11

---
+++ solution.js
@@ -0,0 +1,37 @@
+const äpfel = 20
+const orange = 30
+const ergebnis1 = äpfel===orange
+//1
+console.log({ergebnis1})
+//2
+const ergebnis2 = äpfel!==orange
+console.log({ergebnis2})
+//3
+const ergebnis3 = äpfel>orange
+console.log({ergebnis3})
+//4
+const ergebnis4=äpfel<orange
+console.log({ergebnis4})
+//5
+const ergebnis5=orange>äpfel
+console.log({ergebnis5})
+//6
+const mangos = 5
+const manÄpf = mangos*äpfel
+const manor = mangos+orange
+const ergebnis6=manÄpf>manor
+console.log({ergebnis6})
+//7
+const mangminäpf = mangos-äpfel
+const oradivman = orange/mangos
+const ergebnis7 = mangminäpf<oradivman
+console.log({ergebnis7})
+//8
+const ergebnis8= mangos===äpfel===orange
+console.log({ergebnis8})
+//9
+const ergebnis9 = äpfel/mangos===orange/mangos
+console.log({ergebnis9})
+//10
+const ergebnis10 = mangos+äpfel>orange-mangos
+console.log({ergebnis10})
